44d025e5d9965aaf29fbb73462814b2238df0925
Darknifus <darknifus339@gmail.com>
2026-05-27 19:01:17 +0700

    Initial commit for DarkSelf fork

---
+++ examples/reply.py
@@ -0,0 +1,19 @@
+import discord_self
+
+
+class MyClient(discord_self.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+client = MyClient()
+client.run('token')
